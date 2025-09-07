5a60309e253b1c1ef51bfd4bcbb54e291b17c8b4
laogia201 <o0oking.dark@gmail.com>
2025-09-07 10:42:26 +0700

    edit cors

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Thêm dòng này
+app.use(cors({
+    origin: "https://hdmovie-seven.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+app.options("*", cors());
 app.use(express.json());
 
 // Database connection
